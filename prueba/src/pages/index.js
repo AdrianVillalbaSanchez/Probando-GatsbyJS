811aa91857affb40bbdd7a8f11271a449a7c5f24
Adrián Villalba Sánchez <adrianvillalbasanchez@gmail.com>
2018-08-23 19:16:51 +0200

    map function to display blog's index has been created

--- prueba/src/pages/index.js
+++ prueba/src/pages/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link><br/>
-    <Link to="/page-3/">Go to page 3</Link>
-
+    <h2>Index</h2>
+    {data.allMarkdownRemark.edges.map(post => (
+      <a href={post.node.frontmatter.path}>{post.node.frontmatter.title}</a>
+    ))}
   </div>
 )
 
