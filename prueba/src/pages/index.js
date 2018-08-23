80003ff9a11114e43049c4dd01b55fbdb0ba9f16
Adrián Villalba Sánchez <adrianvillalbasanchez@gmail.com>
2018-08-23 19:28:19 +0200

    "a"  to react-link

--- prueba/src/pages/index.js
+++ prueba/src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = ({data}) => (
     <Link to="/page-2/">Go to page 2</Link><br/>
     <h2>Index</h2>
     {data.allMarkdownRemark.edges.map(post => (
-      <a key={post.node.id} href={post.node.frontmatter.path}>{post.node.frontmatter.title}</a>
+      <Link key={post.node.id} href={post.node.frontmatter.path}>{post.node.frontmatter.title}</Link>
     ))}
   </div>
 )
