6b5117d6dac1bdd165bfe596277bc2421fdde73e
Adrián Villalba Sánchez <adrianvillalbasanchez@gmail.com>
2018-08-23 19:18:42 +0200

    id to each blog added

--- prueba/src/pages/index.js
+++ prueba/src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = ({data}) => (
     <Link to="/page-2/">Go to page 2</Link><br/>
     <h2>Index</h2>
     {data.allMarkdownRemark.edges.map(post => (
-      <a href={post.node.frontmatter.path}>{post.node.frontmatter.title}</a>
+      <a key={post.node.id} href={post.node.frontmatter.path}>{post.node.frontmatter.title}</a>
     ))}
   </div>
 )
@@ -19,6 +19,7 @@ export const pageQuery = graphql`
     allMarkdownRemark(limit: 1000){
       edges{
         node{
+          id
           frontmatter{
             path
             title
